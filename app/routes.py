cec1018b9e92c0017d7ca38682a41ca1bc68537f
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-07-26 15:56:12 -0500

    Add get all garments endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
 from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
+from app.models.Garment import Garment
 
 # board_bp = Blueprint('board_bp', __name__, url_prefix="/boards/")
 garment_bp = Blueprint('garment_bp', __name__, url_prefix="/garments")
 
 # get all garments
+@garment_bp.route("", methods=["GET"])
+def get_all_garments():
+    garments = Garment.query.all()
+    garments_response = [garment.to_dict() for garment in garments]
+
+    return jsonify(garments_response)
 
 # create one new garment listing
 
