2fee95503965f5db548b9d7f5ffcfa89b9ce9eed
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-08-16 10:44:11 -0500

    Add POST, GET tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
+from app.models.Garment import Garment
 
 
 @pytest.fixture
@@ -23,3 +24,29 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_garments(app):
+    # Arrange
+    garment1 = Garment(
+        title="Arabella Gown",
+        brand="BHLDN",
+        size=8,
+        color="ivory",
+        condition="Excellent used condition",
+        price="800",
+        description="A beautiful, flowy blush gown"
+    )
+
+    garment2 = Garment(
+        title="Lenox Short Suit",
+        brand="Christian Siriano",
+        size=16,
+        color="black",
+        condition="New with tags",
+        price="400",
+        description="A fresh, modern take on the courthouse wedding suit"
+    )
+
+    db.session.add_all([garment1, garment2])
+    db.session.commit()
---
+++ tests/test_routes.py
@@ -0,0 +1,43 @@
+def test_get_all_garments_no_records(client):
+    # Act
+    response = client.get("/garments")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_one_garment(client, two_saved_garments):
+    # Act
+    response = client.get("/garments/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "title": "Arabella Gown",
+        "brand": "BHLDN",
+        "size": 8,
+        "color": "ivory",
+        "condition": "Excellent used condition",
+        "price": "800",
+        "description": "A beautiful, flowy blush gown"
+    }
+
+def test_create_one_garment(client):
+    # Act
+    response = client.post("/garments", json={
+        "title": "Solaine Gown",
+        "brand": "Leanne Marshall",
+        "size": 18,
+        "color": "ivory",
+        "condition": "Good used condition",
+        "price": "600",
+        "description": "A watercolor-inspired gown"
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body.title == "Solaine Gown"
